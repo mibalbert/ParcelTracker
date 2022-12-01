b640efcf18bf51f77043a98dc443b3cbce82e0df
Alberto <mib.albert@gmail.com>
2022-12-01 21:36:51 +0200

    unreg fcii

--- public/js/real-home.js
+++ public/js/real-home.js
@@ -15,8 +15,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		const map = new google.maps.Map(document.getElementById('map-bitch'), {
 			// mapId: 'b1beacae401d047c',
-			mapId: '5b376c6ce00e84eb',
-			// mapId: '14558a00a81bc942',
+			// mapId: '5b376c6ce00e84eb',
+			mapId: '14558a00a81bc942',
 			mapTypeControl: false,
 			disableDefaultUI: true,
 			center: { lat: 52.713709, lng: -1.586320 },
@@ -253,7 +253,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			this.map.setCenter(bounds.getCenter()); //or use custom center
 			this.map.fitBounds(bounds);
 			this.map.setZoom(this.map.getZoom() - 0.8);
-		
 		}
 
 		drawDashedCurve(m1, m2, map) {
